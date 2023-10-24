2d25eabdedbb20f2a3079022e29109b0eaedb03a
soulmate <egor.unknown05@mail.ru>
2023-10-25 00:56:17 +0300

    Добавляет проверку на аудиторию и дисциплину через hashSet. Реализует проверку на правильность ввода ФИО.

--- Timetable/MainWindow.xaml.cs
+++ Timetable/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Timetable
@@ -10,33 +12,72 @@ namespace Timetable
 		{
 			InitializeComponent();
 		}
-        
+
+
+		// Не дает создать две одинаковые аудитории
+		HashSet<string> audienceSet = new HashSet<string>();
+
 		private void AddAudienceClick(object sender, RoutedEventArgs e)
 		{
-			cbAudience.Items.Add(tAudition.Text.Trim());
+			var newAudience = tAudition.Text.Trim();
+
+			// Проверить, есть ли уже такой элемент в HashSet
+			if (!audienceSet.Contains(newAudience))
+			{
+				// Если нет, добавить его в HashSet и ComboBox
+				audienceSet.Add(newAudience);
+				cbAudience.Items.Add(newAudience);
+			}
+
 			tAudition.Clear();
 		}
-		
+
 		private void AddTeacherClick(object sender, RoutedEventArgs e)
 		{
-			cbTeacher.Items.Add(tTeacher.Text.Trim());
+			var teacher = tTeacher.Text.Trim();
+			var words = teacher.Split(' ');
+
+			// Проверить, что введено ровно 3 слова и каждое из них имеет минимум 2 символа
+			if (words.Length == 3 && words.All(word => word.Length >= 2))
+			{
+				// Сделать первую букву каждого слова заглавной, остальные прописные
+				teacher = string.Join(" ", words.Select(word => char.ToUpper(word[0]) + word.Substring(1).ToLower()));
+
+				// Проверить, есть ли уже такой элемент в ComboBox
+				if (cbTeacher.Items.IndexOf(teacher) == -1)
+				{
+					// Если нет, добавить его
+					cbTeacher.Items.Add(teacher);
+				}
+			}
+
 			tTeacher.Clear();
 		}
-		
+
+		HashSet<string> disciplineSet = new HashSet<string>();
+
 		private void AddDisciplineClick(object sender, RoutedEventArgs e)
 		{
-			cbDiscipline.Items.Add(tDiscipline.Text.Trim());
+			var newDiscipline = tDiscipline.Text.Trim();
+
+			if (!disciplineSet.Contains(newDiscipline))
+			{
+				disciplineSet.Add(newDiscipline);
+				cbDiscipline.Items.Add(newDiscipline);
+			}
+
 			tDiscipline.Clear();
 		}
-        
+
 		private void onCreate(object sender, RoutedEventArgs e)
 		{
 			var audience = cbAudience.Text.Trim();
 			var teacher = cbTeacher.Text.Trim();
 			var discipline = cbDiscipline.Text.Trim();
 
-			// Проверить, что все поля ComboBox заполнены
-			if (string.IsNullOrWhiteSpace(audience) || string.IsNullOrWhiteSpace(teacher) || string.IsNullOrWhiteSpace(discipline))
+			// Проверка, что все поля ComboBox заполнены
+			if (string.IsNullOrWhiteSpace(audience) || string.IsNullOrWhiteSpace(teacher) ||
+			    string.IsNullOrWhiteSpace(discipline))
 			{
 				return;
 			}
